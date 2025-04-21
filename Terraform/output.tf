5ee30b8fa493136eaff3eac60d6102764d879f00
Pratham verma <vermapratham623@gmail.com>
2025-04-21 11:55:55 +0530

    add clean directory of terraform

---
+++ Terraform/output.tf
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
+=======
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
+}
+>>>>>>> 169bf990666589754b8a9569d7ea261a5d1e4904
---
+++ Terraform/variable.tf
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+variable "instance_name" {
+  description = "Name for the EC2 instance"
+  type        = string
+  default     = "cloud-window-tf"
+}
+=======
+variable "instance_name" {
+  description = "Name for the EC2 instance"
+  type        = string
+  default     = "cloud-window-tf"
+}
+>>>>>>> 169bf990666589754b8a9569d7ea261a5d1e4904
